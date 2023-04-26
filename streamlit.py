c95f0bd808170def1bf963fc04bc0fbe3574937f
kieuhuy <83636991+kieuhuy@users.noreply.github.com>
2023-04-26 16:17:52 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 
 import streamlit as st
+from streamlit_option_menu import option_menu
 
 st.set_page_config(
     page_title="Thesis",
@@ -28,3 +29,26 @@ st.markdown(
     - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
 """
 )
+with st.sidebar:
+    selected = option_menu(
+        menu_title = "Main Menu",
+        options =["Home", "Info","Result"], 
+    )
+if selected == "Home":
+    st.write("# Welcome to my thesis demo! 👋")
+if selected == "Info":
+    st.markdown(
+    """
+    An interactive application that displays the results of my project and provides information on the tools used in it
+    ### Want to learn more?
+    - Check out [streamlit.io](https://streamlit.io)
+    - Jump into our [documentation](https://docs.streamlit.io)
+    - Ask a question in our [community
+        forums](https://discuss.streamlit.io)
+    ### See more complex demos
+    - Use a neural net to [analyze the Udacity Self-driving Car Image
+        Dataset](https://github.com/streamlit/demo-self-driving)
+    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+"""
+)
+    
